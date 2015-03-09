8c8f71e02923450d182d28b2b8314a386d7a398b
Jessica Lynn Suttles <jlsuttles@gmail.com>
2015-03-09 07:34:32 -0700

    Use cypress's SyslogString method

--- loggly/loggly.go
+++ loggly/loggly.go
@@ -2,15 +2,10 @@ package loggly
 
 import (
 	"bytes"
-	"time"
 
-	"code.google.com/p/go-uuid/uuid"
 	"github.com/vektra/addons/lib/tcplog"
 	"github.com/vektra/cypress"
 )
-
-const cTimeFormat = time.RFC3339Nano
-const cNewline = "\n"
 
 type LogglyFormatter struct {
 	Token string
@@ -24,43 +19,7 @@ func NewLogger(address string, ssl bool, token string, pen string) *tcplog.Logge
 func (lf *LogglyFormatter) Format(m *cypress.Message) ([]byte, error) {
 	var buf bytes.Buffer
 
-	time := m.GetTimestamp().Time().Format(cTimeFormat)
-
-	buf.WriteString(time)
-	buf.WriteString(" ")
-
-	if s := m.GetSessionId(); len(s) > 0 {
-		buf.WriteString(uuid.UUID(s).String()[0:7])
-	} else {
-		buf.WriteString("0000000")
-	}
-
-	// Special case the logs that come out of the volts to make them easier to read
-	if m.GetType() == 0 {
-		if volt, ok := m.GetString("volt"); ok {
-			if log, ok := m.GetString("log"); ok {
-
-				buf.WriteString(" ")
-				buf.WriteString(volt)
-				buf.WriteString(" ")
-				buf.WriteString(log)
-
-				buf.WriteString("[")
-				buf.WriteString(lf.Token)
-				buf.WriteString("@")
-				buf.WriteString(lf.PEN)
-				buf.WriteString("]")
-
-				buf.WriteString(cNewline)
-
-				return buf.Bytes(), nil
-			}
-		}
-	}
-
-	buf.WriteString(" system ")
-	buf.WriteString("*")
-	buf.WriteString(m.KVPairs())
+	buf.WriteString(m.SyslogString(false, false))
 
 	buf.WriteString("[")
 	buf.WriteString(lf.Token)
@@ -68,7 +27,7 @@ func (lf *LogglyFormatter) Format(m *cypress.Message) ([]byte, error) {
 	buf.WriteString(lf.PEN)
 	buf.WriteString("]")
 
-	buf.WriteString(cNewline)
+	buf.WriteString("\n")
 
 	return buf.Bytes(), nil
 }
--- papertrail/papertrail.go
+++ papertrail/papertrail.go
@@ -2,15 +2,10 @@ package papertrail
 
 import (
 	"bytes"
-	"time"
 
-	"code.google.com/p/go-uuid/uuid"
 	"github.com/vektra/addons/lib/tcplog"
 	"github.com/vektra/cypress"
 )
-
-const cTimeFormat = time.RFC3339Nano
-const cNewline = "\n"
 
 type PapertrailFormatter struct{}
 
@@ -21,37 +16,9 @@ func NewLogger(address string, ssl bool) *tcplog.Logger {
 func (pf *PapertrailFormatter) Format(m *cypress.Message) ([]byte, error) {
 	var buf bytes.Buffer
 
-	time := m.GetTimestamp().Time().Format(cTimeFormat)
+	buf.WriteString(m.SyslogString(false, false))
 
-	buf.WriteString(time)
-	buf.WriteString(" ")
-
-	if s := m.GetSessionId(); len(s) > 0 {
-		buf.WriteString(uuid.UUID(s).String()[0:7])
-	} else {
-		buf.WriteString("0000000")
-	}
-
-	// Special case the logs that come out of the volts to make them easier to read
-	if m.GetType() == 0 {
-		if volt, ok := m.GetString("volt"); ok {
-			if log, ok := m.GetString("log"); ok {
-
-				buf.WriteString(" ")
-				buf.WriteString(volt)
-				buf.WriteString(" ")
-				buf.WriteString(log)
-				buf.WriteString(cNewline)
-
-				return buf.Bytes(), nil
-			}
-		}
-	}
-
-	buf.WriteString(" system ")
-	buf.WriteString("*")
-	buf.WriteString(m.KVPairs())
-	buf.WriteString(cNewline)
+	buf.WriteString("\n")
 
 	return buf.Bytes(), nil
 }
